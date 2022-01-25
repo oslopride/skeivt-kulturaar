cf619e05aa50b0484ca5a942361bc79c8b65d1a4
camorte <carloalfredo.morte@gmail.com>
2022-01-25 08:37:14 +0100

    fix wrong imports and run linting

--- sanity/actions/index.js
+++ sanity/actions/index.js
@@ -1,14 +1,14 @@
 import defaultResolve from "part:@sanity/base/document-actions";
-import { approveExternalEventEditRequestAction } from "./workflow/approveeventRequestAction";
-import { declineExternalEventEditRequestAction } from "./workflow/declineeventRequestAction";
+import { approveExternalEventEditRequestAction } from "./workflow/approveExternalEventRequestAction";
+import { declineExternalEventEditRequestAction } from "./workflow/declineExternalEventRequestAction";
 
 export default function resolveDocumentActions(props) {
-	if (props.type === "eventRequest") {
-		return [
-			approveExternalEventEditRequestAction,
-			declineExternalEventEditRequestAction
-		];
-	}
+  if (props.type === "eventRequest") {
+    return [
+      approveExternalEventEditRequestAction,
+      declineExternalEventEditRequestAction,
+    ];
+  }
 
-	return [...defaultResolve(props)];
+  return [...defaultResolve(props)];
 }
