edb4de0617ec0271d9093aa849df084b1a307364
Roger Andersen <hexjelly@hexjelly.com>
2022-01-30 17:29:56 +0100

    wip

--- web/pages/submit.tsx
+++ web/pages/submit.tsx
@@ -1,7 +1,7 @@
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import Multiselect from 'multiselect-react-dropdown';
+import { MultiSelect, NativeSelect } from '@mantine/core';
 
 import { Calendar } from '../components/icons/calendar';
 import { Info } from '../components/icons/info';
@@ -23,13 +23,28 @@ const COUNTIES = [
   'Vestland',
   'Trøndelag',
   'Troms og Finnmark',
-] as const;
+];
+
+const EVENT_TYPES = [
+  'Musikk',
+  'Dans',
+  'Teater',
+  'Drag',
+  'Film',
+  'Litteratur / Poesi',
+  'Samtale / Debatt',
+  'Workshop',
+  'Andre kunstformer',
+];
+
+const EVENT_FILTERS = ['Universelt utformet', 'Tegnespråktolk', 'WC', 'Rusfritt'];
 
 export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPropsType<typeof getStaticProps>) {
   const [ageLimit, setAgelimit] = useState(false);
   const [isPhysical, setIsPhysical] = useState(true);
   const [isDigital, setIsDigital] = useState(false);
-  const [county, setCounty] = useState<typeof COUNTIES[number]>();
+  const [eventTypes, setEventTypes] = useState<Array<typeof EVENT_TYPES[number]>>([]);
+  const [eventFilters, setEventFilters] = useState<Array<typeof EVENT_FILTERS[number]>>([]);
 
   const { getRootProps, getInputProps } = useDropzone({
     maxSize: 5000000,
@@ -47,12 +62,16 @@ export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPr
           const target = e.target as HTMLFormElement;
           try {
             const formData = new FormData(target);
-            formData.append('county', county && county[0] ? county[0] : '');
+            formData.append('eventTypes', JSON.stringify(eventTypes));
+            formData.append('eventFilters', JSON.stringify(eventFilters));
 
-            fetch('/api/submit', {
-              method: 'POST',
-              body: formData,
-            });
+            // fetch('/api/submit', {
+            //   method: 'POST',
+            //   body: formData,
+            // });
+            for (const t of formData.entries()) {
+              console.log(t);
+            }
           } catch (err) {
             console.log('Error:', err);
           }
@@ -69,16 +88,41 @@ export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPr
           <label htmlFor="name" aria-required="true">
             Navn på arrangementet
           </label>
-          <input name="name" required placeholder="Navn på arrangementet" />
+          <input name="name" required placeholder="Navn på arrangementet" className={styles.input} />
 
           <label htmlFor="organizer-name" aria-required="true">
             Arrangørnavn
           </label>
-          <input name="organizer-name" required placeholder="Arrangørnavn" />
+          <input name="organizer-name" required placeholder="Arrangørnavn" className={styles.input} />
 
           <label htmlFor="event-link">Lenke til arrangementet</label>
-          <input name="event-link" placeholder="URL" aria-describedby="event-link-help-text" />
+          <input name="event-link" placeholder="URL" aria-describedby="event-link-help-text" className={styles.input} />
           <small id="event-link-help-text">Lenke til Facebook arrangement f.eks.</small>
+          <div className={styles.layout}>
+            <div>
+              <MultiSelect
+                className={styles.multiSelect}
+                name="eventTypes"
+                data={EVENT_TYPES}
+                label="Type arrangement"
+                placeholder="Velg type"
+                onChange={setEventTypes}
+                value={eventTypes}
+                required
+              />
+            </div>
+            <div>
+              <MultiSelect
+                className={styles.multiSelect}
+                name="eventFilters"
+                data={EVENT_FILTERS}
+                label="Filter"
+                placeholder="Velg filter"
+                onChange={setEventFilters}
+                value={eventFilters}
+              />
+            </div>
+          </div>
         </fieldset>
         <fieldset>
           <h3>
@@ -92,25 +136,25 @@ export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPr
               <label htmlFor="start-date" aria-required="true">
                 Startdato
               </label>
-              <input name="start-date" required type="date" />
+              <input name="start-date" required type="date" className={styles.input} />
             </div>
             <div>
               <label htmlFor="end-date" aria-required="true">
                 Sluttdato
               </label>
-              <input name="end-date" required type="date" />
+              <input name="end-date" required type="date" className={styles.input} />
             </div>
             <div>
               <label htmlFor="start-time" aria-required="true">
                 Klokkeslett: starttidspunkt
               </label>
-              <input name="start-time" required type="time" />
+              <input name="start-time" required type="time" className={styles.input} />
             </div>
             <div>
               <label htmlFor="end-time" aria-required="true">
                 Klokkeslett: sluttidspunkt
               </label>
-              <input name="end-time" required type="time" />
+              <input name="end-time" required type="time" className={styles.input} />
             </div>
           </div>
         </fieldset>
@@ -128,6 +172,7 @@ export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPr
                 type="checkbox"
                 checked={isPhysical}
                 onChange={(e) => setIsPhysical(e.target.checked)}
+                className={styles.input}
               />
               <label htmlFor="physical">Fysisk</label>
               <input
@@ -135,43 +180,40 @@ export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPr
                 type="checkbox"
                 checked={isDigital}
                 onChange={(e) => setIsDigital(e.target.checked)}
+                className={styles.input}
               />
               <label htmlFor="digital">Digitalt</label>
             </div>
-            {isDigital && (
-              <div>
-                <label htmlFor="digital-event-link">Lenke til digital event</label>
-                <input name="digital-event-link" placeholder="URL" aria-describedby="digital-event-link" />
-              </div>
-            )}
+
             {isPhysical && (
               <>
                 <div>
                   <label htmlFor="address" aria-required={isPhysical}>
                     Addresse
                   </label>
-                  <input name="address" required={isPhysical} />
+                  <input name="address" required={isPhysical} className={styles.input} />
                 </div>
                 <div>
                   <label htmlFor="postalNumber">Postnummer</label>
-                  <input name="postalNumber" />
+                  <input name="postalNumber" className={styles.input} />
+                </div>
+                <div className={styles.gridSpan}>
+                  <NativeSelect name="county" data={COUNTIES} placeholder="Velg fylke" label="Fylke" required />
                 </div>
               </>
             )}
+            {isDigital && (
+              <div className={styles.gridSpan}>
+                <label htmlFor="digital-event-link">Lenke til digital event</label>
+                <input
+                  name="digital-event-link"
+                  placeholder="URL"
+                  aria-describedby="digital-event-link"
+                  className={styles.input}
+                />
+              </div>
+            )}
           </div>
-          {isPhysical && (
-            <div className={styles.gridSpan}>
-              <label htmlFor="county">Fylke</label>
-              <Multiselect
-                options={COUNTIES}
-                placeholder="Velg fylke"
-                singleSelect
-                isObject={false}
-                selectedValues={county}
-                onSelect={(item) => setCounty(item)}
-              />
-            </div>
-          )}
         </fieldset>
         <fieldset>
           <h3>
@@ -188,6 +230,7 @@ export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPr
                 type="checkbox"
                 checked={ageLimit}
                 onChange={() => setAgelimit((prev) => !prev)}
+                className={styles.input}
               />{' '}
               Ja
             </div>
@@ -196,24 +239,30 @@ export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPr
                 <label htmlFor="age-limit-age" aria-required={ageLimit}>
                   Spesifiser alder
                 </label>
-                <input name="age-limit-age" placeholder="18" type="number" required={ageLimit} />
+                <input
+                  name="age-limit-age"
+                  placeholder="18"
+                  type="number"
+                  required={ageLimit}
+                  className={styles.input}
+                />
               </div>
             )}
             <div>
               <label htmlFor="ticket-price">Billettpris</label>
-              <input name="ticket-price" placeholder="kr" />
+              <input name="ticket-price" placeholder="kr" className={styles.input} />
             </div>
             <div className={styles.checkboxContainer}>
-              <input name="ticket-free" type="checkbox" />
+              <input name="ticket-free" type="checkbox" className={styles.input} />
               <label htmlFor="ticket-free">Gratis</label>
             </div>
             <div>
               <label htmlFor="ticket-purchase-link">Eventuell lenke til billettkjøp:</label>
-              <input name="ticket-purchase-link" placeholder="URL" />
+              <input name="ticket-purchase-link" placeholder="URL" className={styles.input} />
             </div>
             <div>
               <label htmlFor="event-info">Om arrangementet</label>
-              <textarea name="event-info" placeholder="Om arrangementet" />
+              <textarea name="event-info" placeholder="Om arrangementet" className={styles.input} />
             </div>
             <label htmlFor="image">Last opp bilde</label>
             <div>
@@ -248,34 +297,34 @@ export default function SubmitEvent({ image, title, subTitle }: InferGetStaticPr
               <label htmlFor="contact-name" aria-required>
                 Fullt navn
               </label>
-              <input name="contact-name" placeholder="Fullt navn" required />
+              <input name="contact-name" placeholder="Fullt navn" required className={styles.input} />
             </div>
             <div>
               <label htmlFor="pronoun">Pronomen</label>
-              <input name="pronoun" placeholder="Pronomen" />
+              <input name="pronoun" placeholder="Pronomen" className={styles.input} />
             </div>
             <div>
               <label htmlFor="phone-number" aria-required>
                 Telefonnummer
               </label>
-              <input name="phone-number" placeholder="Telefonnummer" required />
+              <input name="phone-number" placeholder="Telefonnummer" required className={styles.input} />
             </div>
             <div>
               <label htmlFor="contact-email">E-postaddresse</label>
-              <input name="contact-email" placeholder="E-postaddresse" />
+              <input name="contact-email" placeholder="E-postaddresse" className={styles.input} />
             </div>
             <div className={styles.gridSpan}>
               <label htmlFor="contact-info">Informasjon til redaktør</label>
-              <textarea name="contact-info" placeholder="Informasjon til redaktør" />
+              <textarea name="contact-info" placeholder="Informasjon til redaktør" className={styles.input} />
             </div>
           </div>
         </fieldset>
         <fieldset>
           <label htmlFor="agree">Jeg samtykker til at min persondata blir lagret</label>
-          <input type="checkbox" name="agree" />
+          <input type="checkbox" name="agree" className={styles.input} />
         </fieldset>
         <p>
-          <button>Send inn arrangement</button>
+          <button className={styles.button}>Send inn arrangement</button>
         </p>
         <input type="text" name="password" className={styles.form_hp} tabIndex={-1} autoComplete="off" />
         <input type="text" name="username" className={styles.form_hp} tabIndex={-1} autoComplete="off" />
