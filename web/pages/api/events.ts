766bb791e0202cbb2d2cde53f3a90f8f8e9029c2
Roger Andersen <hexjelly@hexjelly.com>
2022-02-05 20:17:40 +0100

    make event for each event date

--- web/pages/api/events.ts
+++ web/pages/api/events.ts
@@ -2,7 +2,7 @@ import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 import sanity, { urlFor } from '../../sanity';
 import { PublicSanityEvent } from '../../types/sanity';
-// Initializing the cors middleware
+
 const cors = Cors({
   methods: ['GET', 'HEAD'],
 });
@@ -21,23 +21,38 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
   });
 }
 
+type APIEvent = Omit<PublicSanityEvent, 'image' | 'eventDates'> & {
+  image: { full: string | null; thumbnail: string | null };
+  eventStart: string;
+  eventEnd: string;
+};
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   await runMiddleware(req, res, cors);
 
   const fields =
     'address,ageLimit,county,digitalEventUrl,eventDates,eventDescription,eventFilters,eventLink,eventName,eventTypes,image,postalCode,ticketPrice';
   const query = `*[_type == 'eventRequest' && approved == true]{${fields}}`;
-  const events: Array<PublicSanityEvent> = await sanity.fetch(query);
-  const eventsWithImageUrls = events.map((event) => {
-    const { image, ...rest } = event;
-    const thumbnail = image ? urlFor(image).width(400).url().toString() : null;
-    const full = image ? urlFor(image).url().toString() : null;
-    const images = { thumbnail, full };
-    return { ...rest, image: images };
-  });
+  const sanityEvents: Array<PublicSanityEvent> = await sanity.fetch(query);
 
-  // Rest of the API logic
-  res.json(eventsWithImageUrls);
+  const events: Array<APIEvent> = [];
+  for (const event of sanityEvents) {
+    // for each date of an event, create a new event
+    for (const { eventEnd, eventStart } of event.eventDates || []) {
+      const { image, eventDates, ...oldEvent } = event;
+      const thumbnail = image ? urlFor(image).width(400).url().toString() : null;
+      const full = image ? urlFor(image).url().toString() : null;
+      const images = { thumbnail, full };
+      const newEvent: APIEvent = {
+        ...oldEvent,
+        image: images,
+        eventStart,
+        eventEnd,
+      };
+      events.push(newEvent);
+    }
+  }
+
+  res.json(events);
 }
 
 export default handler;
