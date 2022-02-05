f1c77f2b14d1b77228e8c1a6710b5a380da9dac1
Roger Andersen <hexjelly@hexjelly.com>
2022-02-05 18:27:41 +0100

    responsive fixes (#41)
    * add new font
    
    * horrible css to try and fix responsive stuff
    
    * fix

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,8 +1,22 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { createGetInitialProps } from '@mantine/next';
 
 const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="https://use.typekit.net/rmr1bwf.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
