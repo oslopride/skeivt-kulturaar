0e7cc0a9c3fe44448ea12f1567fbe1ab983faec2
Roger Andersen <hexjelly@hexjelly.com>
2022-02-05 21:28:58 +0100

    see if semi-caching works in lambda functions

--- web/pages/api/events.ts
+++ web/pages/api/events.ts
@@ -21,6 +21,9 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
   });
 }
 
+// ghetto semi-cache, better than nothing
+let cache: Array<APIEvent> = [];
+
 type APIEvent = Omit<PublicSanityEvent, 'image' | 'eventDates'> & {
   image: { full: string | null; thumbnail: string | null };
   eventStart: string;
@@ -28,31 +31,35 @@ type APIEvent = Omit<PublicSanityEvent, 'image' | 'eventDates'> & {
 };
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   await runMiddleware(req, res, cors);
+  if (cache.length === 0) {
+    const fields =
+      'address,ageLimit,county,digitalEventUrl,eventDates,eventDescription,eventFilters,eventLink,eventName,eventTypes,image,postalCode,ticketPrice';
+    const query = `*[_type == 'eventRequest' && approved == true]{${fields}}`;
+    const sanityEvents: Array<PublicSanityEvent> = await sanity.fetch(query);
 
-  const fields =
-    'address,ageLimit,county,digitalEventUrl,eventDates,eventDescription,eventFilters,eventLink,eventName,eventTypes,image,postalCode,ticketPrice';
-  const query = `*[_type == 'eventRequest' && approved == true]{${fields}}`;
-  const sanityEvents: Array<PublicSanityEvent> = await sanity.fetch(query);
-
-  const events: Array<APIEvent> = [];
-  for (const event of sanityEvents) {
-    // for each date of an event, create a new event
-    for (const { eventEnd, eventStart } of event.eventDates || []) {
-      const { image, eventDates, ...oldEvent } = event;
-      const thumbnail = image ? urlFor(image).width(400).url().toString() : null;
-      const full = image ? urlFor(image).url().toString() : null;
-      const images = { thumbnail, full };
-      const newEvent: APIEvent = {
-        ...oldEvent,
-        image: images,
-        eventStart,
-        eventEnd,
-      };
-      events.push(newEvent);
+    const events: Array<APIEvent> = [];
+    for (const event of sanityEvents) {
+      // for each date of an event, create a new event
+      for (const { eventEnd, eventStart } of event.eventDates || []) {
+        const { image, eventDates, ...oldEvent } = event;
+        const thumbnail = image ? urlFor(image).width(400).url().toString() : null;
+        const full = image ? urlFor(image).url().toString() : null;
+        const images = { thumbnail, full };
+        const newEvent: APIEvent = {
+          ...oldEvent,
+          image: images,
+          eventStart,
+          eventEnd,
+        };
+        events.push(newEvent);
+      }
     }
+    cache = events;
+    res.json(events);
+  } else {
+    console.log('using cache');
+    res.json(cache);
   }
-
-  res.json(events);
 }
 
 export default handler;
